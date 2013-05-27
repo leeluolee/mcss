818c2b70a1b83058bbb49ee3f82621368a29ee4b
leeluolee <87399126@163.com>
2013-05-27 11:49:45 +0800

    add mcss bin

--- test/specs/test-parser.js
+++ test/specs/test-parser.js
@@ -7,12 +7,15 @@ var it = mcss.interpreter;
 
 this.parser = {}
 
+mcss.interpret(input, {}, function(){
+    
+})
 
 http('../data/simple.mcss',function(text){
     var date = +new Date()
     console.log(tk.tokenize(text))
     ps.parse(text, {}, function(error, ast){
-        console.log(ast)
+        console.log(ast                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             )
         console.log(+new Date - date)
     })
     // var text = mcss.parse(text, {
