55920a349cfb0a9efdb26a24f2a2133846e8c27d
leeluolee <87399126@163.com>
2013-06-14 16:48:19 +0800

    delete som file

--- test/nodejs/import.js
+++
@@ -1,9 +0,0 @@
-var mcss = require('../../lib/mcss')
-var path = require('path');
-var fs = require('fs');
-
-
-mcss.io.parse('/home/luobo/code/mcss/test/data/parse.mcss')
-    .always(function(ast){
-        console.log(ast);
-    })
