8bfa064d542d3ca4bad26e01fa7ee5972cc8acb0
leeluolee <87399126@163.com>
2013-06-29 19:23:27 +0800

    add csscss.js example

--- test/fixtures/csscss.js
+++ test/fixtures/csscss.js
@@ -6,15 +6,41 @@
 var mcss = require('../../');
 var path = require('path');
 var node = mcss.node;
-var color = require('../../lib/helper/color');
-// sign: [ruleset]
-// 内存还速度
+// 改变颜色帮助识别console.log
+var color = mcss.helper.color;
 
-var rulesets = [], MAX_DUPLICATES = 6;
 
+var rulesets = [], 
+    // 允许的delaration最大重复率
+    MAX_DUPLICATES = 6,
+    // 自定义walker，在这里我们只对rulset感兴趣
+    walkers = {
+        'ruleset': function(ast){
+            // 获取ruleset的块中的declaration列表
+            var list = ast.block.list,
+                selector = node.toStr(ast.selector),
+                res = {
+                    selector:selector, 
+                    filename:ast.filename,
+                    lineno: ast.lineno,
+                    map:{}
+                },
+                sign, map = res.map;
+
+            list.forEach(function(declaration){
+
+                if(declaration.type === 'declaration'){
+
+                    sign = node.toStr(declaration.property)+':'+mcss.node.toStr(declaration.value);
+                    if(!map[sign]) map[sign] = true;
+                }
+            });
+
+            rulesets.push(res);
+        }
+    }
 // use a map, achieve O(n) to find duplicates.
 function findDupls(a, b){
-    // 以a为准绳
     var alist = a.map,
         blist = b.map,
         duplicates = [];
@@ -25,51 +51,38 @@ function findDupls(a, b){
     return duplicates;
 }
 
+
+// mcss 实例创建
 var instance = mcss({
-    // 这个css 来自 github.com 官网
-    filename: path.join(__dirname, '../mcss/_large.mcss'),
-    walkers: [{
-        'ruleset': function(ast){
-            var list = ast.block.list,
-                selector = node.toStr(ast.selector),
-                res = {
-                    selector:selector, 
-                    filename:ast.filename,
-                    lineno: ast.lineno,
-                    map:{}
-                },
-                sign, map = res.map;
-            list.forEach(function(declaration){
-                if(declaration.type === 'declaration'){
-                    sign = node.toStr(declaration.property)+':'+mcss.node.toStr(declaration.value);
-                    if(!map[sign]) map[sign] = true;
-                }
-            })
-            rulesets.push(res);
-        }
-    }]
+    // 这个css 来自 github.com 官网 的css  ... 重复率惊人
+    filename: path.join(__dirname, '../css/_large.css'),
+    // 传入我们的自定义walker
+    walkers: [walkers]
 })
 
-
-// 获取节点
-var date = +new Date();
+// 所有mcss操作(解释、翻译、词法)都由统一的instance开始
+// 以下是解释
 instance.interpret().done(function(ast){
     var len = rulesets.length, 
         mapa, mapb, jlen, duplicates;
+
     for(; len-- ;){
+
         jlen = len; 
         mapa = rulesets[len];
+
         for(; jlen--;){
             mapb = rulesets[jlen];
             duplicates = findDupls(mapa, mapb);
-            if(duplicates.length > MAX_DUPLICATES)
+            if(duplicates.length > MAX_DUPLICATES){
                 console.log(
                     color(mapa.selector, 'red') + ' at ('+ color(mapa.filename + ':' + mapa.lineno, 'yellow') + ') and \n' +
                     color(mapb.selector, 'red') + ' at ('+ color(mapb.filename + ':' + mapb.lineno, 'yellow') + ') has ' + color(duplicates.length, 'blue') + ' duplicates:\n\t' +
                     duplicates.join(',\n\t')
                     )
+            }
         }
     }
 }).fail(function(err){
-    console.log(err)
+    throw err;
 })
