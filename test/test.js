b37549c6532bce51634114c85a84744e4e9d5c02
leeluolee <87399126@163.com>
2013-07-25 21:49:03 +0800

    support apply

--- test/test.js
+++ test/test.js
@@ -11,13 +11,15 @@ var mcss = require('../'),
 
 var not_compare = process.argv[2];
 describe('MCSS TEST', function(){
-    var cases = fs.readdirSync(__dirname + '/mcss').filter(function(file){
-        return /^[^_][-\w]*\.mcss$/.test(file);
-    }).map(function(file){
-        return __dirname + '/mcss/' +file;
-    }).forEach(function(fullpath){
+    console.log()
+    var cases = fs.readdirSync(__dirname + '/mcss').concat(fs.readdirSync(__dirname + '/mcss/issues').map(function(file){
+                return 'issues/' + file;
+            })).filter(function(file){
+        return /(^|\/)[^_][-\w]*\.mcss$/.test(file);
+    }).forEach(function(filename){
+        var fullpath = __dirname + '/mcss/'+filename
         var content = fs.readFileSync(fullpath, 'utf8');
-        var csspath = __dirname+'/css/'+ path.basename(fullpath, '.mcss') +'.css';
+        var csspath = __dirname+'/css/'+ filename.replace('.mcss', '.css');
         try{
             var csscontent = fs.readFileSync(csspath, 'utf8');
         }catch(e){
